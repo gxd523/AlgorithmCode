aa7147194e71c39bd318e4d54db66ebb81afa018
gxd <gxd523@gmail.com>
2021-01-17 22:06:20 +0800

    optimize：BFS

--- src/com/code/datastructure/tree/BreadthFirstSearch.kt
+++ src/com/code/datastructure/tree/BreadthFirstSearch.kt
@@ -6,7 +6,7 @@ import java.util.*
  * 广度优先遍历
  * 利用了队列先进后出的特点，减少了每层队列的创建
  */
-fun breadthTraversal(list: MutableList<MutableList<Int>>, currentLayerNodeQueue: Queue<TreeNode>) {
+fun recurseBfs(list: MutableList<MutableList<Int>>, currentLayerNodeQueue: Queue<TreeNode>) {
     if (currentLayerNodeQueue.isEmpty()) {
         return
     }
@@ -14,17 +14,17 @@ fun breadthTraversal(list: MutableList<MutableList<Int>>, currentLayerNodeQueue:
     val currentLayerValueList = emptyList<Int>().toMutableList()
     for (i in currentLayerNodeQueue.indices) {// 遍历当前层的节点，值存入list，左右子节点顺序存入下一层节点集合
         val node = currentLayerNodeQueue.poll()
-        currentLayerValueList.add(node.value)
+        currentLayerValueList += node.value
 
         node.left?.let { left ->
-            currentLayerNodeQueue.add(left)
+            currentLayerNodeQueue += left
         }
         node.right?.let { right ->
-            currentLayerNodeQueue.add(right)
+            currentLayerNodeQueue += right
         }
     }
-    list.add(currentLayerValueList)
-    breadthTraversal(list, currentLayerNodeQueue)
+    list += currentLayerValueList
+    recurseBfs(list, currentLayerNodeQueue)
 }
 
 /**
@@ -32,16 +32,45 @@ fun breadthTraversal(list: MutableList<MutableList<Int>>, currentLayerNodeQueue:
  * 从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行
  * 递归过程中，函数参数都有当前在第几层，这样每个节点都能根据自己所在层数找到list对应的层子集合
  */
-fun breadthTraversalBetter(list: MutableList<MutableList<Int>>, layer: Int, root: TreeNode?) {
+fun recurseBfsBetter(list: MutableList<MutableList<Int>>, layer: Int, root: TreeNode?) {
     if (root == null) {
         return
     }
     if (list.size == layer) {
-        list.add(emptyArray<Int>().toMutableList())
+        list += emptyArray<Int>().toMutableList()
     }
     list[layer].add(root.value)
-    breadthTraversalBetter(list, layer + 1, root.left)
-    breadthTraversalBetter(list, layer + 1, root.right)
+    recurseBfsBetter(list, layer + 1, root.left)
+    recurseBfsBetter(list, layer + 1, root.right)
+}
+
+/**
+ * 迭代实现广度优先遍历
+ */
+fun iterateBfs(root: TreeNode): List<List<Int>> {
+    val list = emptyList<MutableList<Int>>().toMutableList()
+    list += emptyList<Int>().toMutableList()
+
+    val queue = LinkedList<TreeNode>()
+    queue += root
+
+    var layerCount = 1
+    var count = 0
+
+    while (queue.isNotEmpty()) {
+        if (count < layerCount) {
+            val node = queue.poll()
+            count++
+            list.last() += node.value
+            node.left?.let(queue::add)
+            node.right?.let(queue::add)
+        } else if (count == layerCount) {
+            layerCount = queue.size
+            count = 0
+            list += emptyList<Int>().toMutableList()
+        }
+    }
+    return list
 }
 
 fun main() {
@@ -49,7 +78,7 @@ fun main() {
         .toMutableList()
         .also { list ->
             val root = createTree(intArrayOf(0, 1, 2, 3, 4, 5, 6))
-            breadthTraversalBetter(list, 0, root)
+            recurseBfsBetter(list, 0, root)
         }
         .let(::println)
 
@@ -57,7 +86,9 @@ fun main() {
         .toMutableList()
         .also { list ->
             val root = createTree(intArrayOf(0, 1, 2, 3, 4, 5, 6))
-            breadthTraversal(list, LinkedList<TreeNode>().apply { add(root) })
+            recurseBfs(list, LinkedList<TreeNode>().apply { add(root) })
         }
         .let(::println)
+
+    createTree(intArrayOf(0, 1, 2, 3, 4, 5, 6)).let(::iterateBfs).let(::println)
 }
